f27dd50891580493f46d21fddd617a0f556cbd42
Amir Abrams <amirabrams@mail.com>
2020-05-13 08:49:11 -0500

    Log print version and commit hash

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import webbridge "github.com/amirabrams/webbridge/internal"
+import (
+	"fmt"
+
+	webbridge "github.com/amirabrams/webbridge/internal"
+)
+
+// Version is the WebBridge version number in ISO date format
+var Version string
+
+// GitHash is the git hash tag
+var GitHash string
+
+// BuildDateTime is the date the binary was built
+//var BuildDateTime string
 
 func main() {
+	fmt.Println("Version:", Version, "Hash", GitHash)
 	webbridge.Init()
 }
