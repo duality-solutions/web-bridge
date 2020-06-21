58bdaf6394a730f8c097d8052230fb4b523c98a7
Amir Abrams <amirabrams@mail.com>
2020-06-21 15:32:34 -0500

    Refactor RTC bridge and add String method

--- bridge/bridge.go
+++ bridge/bridge.go
@@ -1,6 +1,10 @@
 package bridge
 
 import (
+	"crypto/sha256"
+	"fmt"
+	"sort"
+
 	"github.com/duality-solutions/web-bridge/rpc/dynamic"
 	"github.com/pion/webrtc/v2"
 )
@@ -22,6 +26,25 @@ const (
 	// StateEstablishRTC offer sent and answer received  = 5
 	StateEstablishRTC
 )
+
+func (s State) String() string {
+	switch s {
+	case StateInit:
+		return "StateInit"
+	case StateNew:
+		return "StateNew"
+	case StateWaitForAnswer:
+		return "StateWaitForAnswer"
+	case StateSendAnswer:
+		return "StateSendAnswer"
+	case StateWaitForRTC:
+		return "StateWaitForRTC"
+	case StateEstablishRTC:
+		return "StateEstablishRTC"
+	default:
+		return "Undefined"
+	}
+}
 
 // Bridge hold information about a link WebRTC bridge connection
 type Bridge struct {
@@ -69,4 +92,49 @@ func NewLinkBridge(requester string, recipient string, acc []dynamic.Account) Br
 		}
 	}
 	return brd
+}
+
+// LinkID returns an hashed id for the link
+func (b Bridge) LinkID() string {
+	var ret string = ""
+	strs := []string{b.MyAccount, b.LinkAccount}
+	sort.Strings(strs)
+	for _, str := range strs {
+		ret += str
+	}
+	hash := sha256.New()
+	hash.Write([]byte(ret))
+	bs := hash.Sum(nil)
+	hs := fmt.Sprintf("%x", bs)
+	return hs
+}
+
+// LinkParticipants returns link participants
+func (b Bridge) LinkParticipants() string {
+	return (b.MyAccount + "-" + b.LinkAccount)
+}
+
+func (b Bridge) String() string {
+	result := fmt.Sprint("Bridge {\nMyAccount: ", b.MyAccount,
+		"\nLinkAccount: ", b.LinkAccount,
+		"\nLinkID: ", b.LinkID(),
+		"\nOffer: ", b.Offer.SDP,
+		"\nAnswer: ", b.Answer.SDP,
+		"\nState: ", b.State.String(),
+		"\nLastPingEpoch: ", b.LastPingEpoch,
+	)
+	if b.PeerConnection != nil {
+		result += fmt.Sprint("\nPeerConnection.ICEConnectionState: ", b.PeerConnection.ICEConnectionState().String(),
+			"\nPeerConnection.ConnectionState: ", b.PeerConnection.ConnectionState().String(),
+		)
+	} else {
+		result += fmt.Sprint("\nPeerConnection.ICEConnectionState: nil\nPeerConnection.ConnectionState: nil")
+	}
+	if b.DataChannel != nil {
+		result += fmt.Sprint("\nDataChannel.ReadyState: ", b.DataChannel.ReadyState().String())
+	} else {
+		result += fmt.Sprint("\nDataChannel.ReadyState: nil")
+	}
+	result += "\n}"
+	return result
 }
