45dac361d56aca6b8e2774c714aa9aedad60f012
amirabrams <amirabrams@mail.com>
2020-08-02 18:59:13 -0500

    Add Dynamic online notification VGP message

--- rpc/dynamic/linkmessage.go
+++ rpc/dynamic/linkmessage.go
@@ -28,7 +28,55 @@ type GetMessageReturnJSON struct {
 	RecordNum      int    `json:"record_num"`
 }
 
-// SendLinkMessage calls the DHT put command to add an encrypted record for the given account link
+// SendNotificationMessage sends an encrypted message to the the given account link using VGP IM
+func (d *Dynamicd) SendNotificationMessage(sender, receiver, message string) (*MessageReturnJSON, error) {
+	var ret MessageReturnJSON
+	cmd := "dynamic-cli link sendmessage " + sender + " " + receiver + " online " + `"` + message + `"`
+	req, err := NewRequest(cmd)
+	if err != nil {
+		return nil, err
+	}
+	res := <-d.ExecCmdRequest(req)
+	err = json.Unmarshal([]byte(res), &ret)
+	if err != nil {
+		return nil, err
+	}
+	return &ret, nil
+}
+
+// GetNotificationMessages calls the local VGP instant message queue
+func (d *Dynamicd) GetNotificationMessages(receiver, sender string) (*[]GetMessageReturnJSON, error) {
+	var ret []GetMessageReturnJSON
+	cmd := "dynamic-cli link getaccountmessages " + receiver + " " + sender + " online"
+	req, err := NewRequest(cmd)
+	if err != nil {
+		return nil, err
+	}
+	res := <-d.ExecCmdRequest(req)
+	var messagesGeneric map[string]interface{}
+	err = json.Unmarshal([]byte(res), &messagesGeneric)
+	if err != nil {
+		util.Error.Println("GetLinkMessages messagesGeneric error", err)
+		return nil, err
+	}
+	for _, v := range messagesGeneric {
+		b, err := json.Marshal(v)
+		if err != nil {
+			util.Error.Println("GetLinkMessages json.Marshal error", err)
+		} else {
+			var message GetMessageReturnJSON
+			err := json.Unmarshal(b, &message)
+			if err != nil {
+				util.Error.Println("GetLinkMessages json.Unmarshal error", err)
+			} else {
+				ret = append(ret, message)
+			}
+		}
+	}
+	return &ret, nil
+}
+
+// SendLinkMessage calls the VGP IM send message command to add an encrypted record for the given account link
 func (d *Dynamicd) SendLinkMessage(sender, receiver, message string) (*MessageReturnJSON, error) {
 	var ret MessageReturnJSON
 	cmd := "dynamic-cli link sendmessage " + sender + " " + receiver + " bridge " + `"` + message + `"`
