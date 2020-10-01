dcb950bfc4cda666447ad9f0815c95a0a4b746e9
amirabrams <amirabrams@mail.com>
2020-10-01 18:02:31 -0500

    Check web config values before updating running settings and file

--- configs/settings/webserver.go
+++ configs/settings/webserver.go
@@ -1,10 +1,22 @@
 package settings
 
-import "github.com/duality-solutions/web-bridge/api/models"
+import (
+	"github.com/duality-solutions/web-bridge/api/models"
+	"github.com/duality-solutions/web-bridge/internal/util"
+)
 
 // UpdateWebServer updates an existing web server settings in current running configuration and file
 func (c *Configuration) UpdateWebServer(web models.WebServerConfig) bool {
 	// check values of web before changing.
+	if !util.IsValidIPAddress(web.BindAddress) {
+		return false
+	}
+	if !util.IsValidCIDRList(web.AllowCIDR) {
+		return false
+	}
+	if web.ListenPort < 1 {
+		return false
+	}
 	c.mut.Lock()
 	defer c.mut.Unlock()
 	c.configFile.WebServer = web
