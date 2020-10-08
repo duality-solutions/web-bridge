9bd0a95f49af4933e8a6bda4833b8733c844695e
Amir Abrams <amirabrams@mail.com>
2020-10-08 02:42:12 -0500

    Fix UI build warnings and Travis CI error

--- web/src/components/WalletFileRestore.tsx
+++ web/src/components/WalletFileRestore.tsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import { Box } from "./ui/Box";
 import { BackButton } from "./ui/Button";
 import { Card } from "./ui/Card";
@@ -14,11 +14,14 @@ export interface WalletFileRestoreProps {
 }
 
 export interface WalletFileRestoreState {
-    stateError?: string;
-    error: DropzoneError | undefined;
+  stateError?: string;
+  error: DropzoneError | undefined;
 }
 
-export class WalletFileRestore extends Component<WalletFileRestoreProps, WalletFileRestoreState> {
+export class WalletFileRestore extends Component<
+  WalletFileRestoreProps,
+  WalletFileRestoreState
+> {
   constructor(props: WalletFileRestoreProps) {
     super(props);
     // bind events
@@ -32,49 +35,92 @@ export class WalletFileRestore extends Component<WalletFileRestoreProps, WalletF
 
   render() {
     const filesSelectedHandler = (files: FilePathInfo[]) => {
-        if (files.length !== 1) {
-            var error: DropzoneError = { title: "More that one file selected", message: "Please select only one file" };
-            this.setState( { error: error });
-            return;
-        }
-        const file: FilePathInfo = files[0];
-        if (file.size > 131072) { //128KiB
-            var error: DropzoneError = { title: "File is too large", message: "Please select a mnemonic recovery file" };
-            this.setState( { error: error });
-            return;
-        }
-        //mnemonicRestoreFilePathSubmitted(file.path);
-        //secureFilePassword();
-        this.props.onComplete();
-    }
+      var error: DropzoneError;
+      if (files.length !== 1) {
+        error = {
+          title: "More that one file selected",
+          message: "Please select only one file"
+        };
+        this.setState({ error: error });
+        return;
+      }
+      const file: FilePathInfo = files[0];
+      if (file.size > 131072) {
+        //128KiB
+        error = {
+          title: "File is too large",
+          message: "Please select a mnemonic recovery file"
+        };
+        this.setState({ error: error });
+        return;
+      }
+      //mnemonicRestoreFilePathSubmitted(file.path);
+      //secureFilePassword();
+      this.props.onComplete();
+    };
 
     return (
       <>
-        <H1 align="center" colored fontWeight="600">Restore Account</H1>
+        <H1 align="center" colored fontWeight="600">
+          Restore Account
+        </H1>
         <Container height="50vh" margin="10% 0 0 0">
-            <form onSubmit={e => {
-                e.preventDefault()
-                e.stopPropagation()
-                return false;
-            }}>
-                <Box direction="column" align="center" width="100%">
-                    <Box direction="column" width="700px" align="start" margin="0 auto 0 auto">
-                        <BackButton onClick={() => this.props.onCancel() } margin="150px 0 0 -80px" />
-                        <Card width="100%" align="center" minHeight="225px" padding="2em 4em 2em 2em">
-                            <Box display="flex" direction="row" margin="0">
-                                <Box width="60px" margin="0">
-                                    <SecureFileIcon width="60px" height="60px" />
-                                </Box>
-                                <Box direction="column" width="500px" align="center" margin="0 auto 0 auto">
-                                    <H3 margin="0 0 1em 0">Restore using Secure Restore File </H3>
-                                    <Dropzone multiple={false} accept={".psh.json"} filesSelected={filesSelectedHandler} error={this.state && this.state.error}></Dropzone>
-                                    {this.state && this.state.stateError ? <Text align="center" color="#e30429">{this.state.stateError}</Text> : <></>}
-                                </Box>
-                            </Box>
-                        </Card>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              return false;
+            }}
+          >
+            <Box direction="column" align="center" width="100%">
+              <Box
+                direction="column"
+                width="700px"
+                align="start"
+                margin="0 auto 0 auto"
+              >
+                <BackButton
+                  onClick={() => this.props.onCancel()}
+                  margin="150px 0 0 -80px"
+                />
+                <Card
+                  width="100%"
+                  align="center"
+                  minHeight="225px"
+                  padding="2em 4em 2em 2em"
+                >
+                  <Box display="flex" direction="row" margin="0">
+                    <Box width="60px" margin="0">
+                      <SecureFileIcon width="60px" height="60px" />
                     </Box>
-                </Box>
-            </form>
+                    <Box
+                      direction="column"
+                      width="500px"
+                      align="center"
+                      margin="0 auto 0 auto"
+                    >
+                      <H3 margin="0 0 1em 0">
+                        Restore using Secure Restore File{" "}
+                      </H3>
+                      <Dropzone
+                        multiple={false}
+                        accept={".psh.json"}
+                        filesSelected={filesSelectedHandler}
+                        error={this.state && this.state.error}
+                      ></Dropzone>
+                      {this.state && this.state.stateError ? (
+                        <Text align="center" color="#e30429">
+                          {this.state.stateError}
+                        </Text>
+                      ) : (
+                        <></>
+                      )}
+                    </Box>
+                  </Box>
+                </Card>
+              </Box>
+            </Box>
+          </form>
         </Container>
       </>
     );
