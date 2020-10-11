0dd8a61e807acd2179db7b33e2acf44380c6e2ca
Amir Abrams <amirabrams@mail.com>
2020-10-11 03:17:53 -0500

    Add default wallet address to account view UI

--- web/src/components/wallet/View.tsx
+++ web/src/components/wallet/View.tsx
@@ -1,28 +1,41 @@
 import React, { Component } from "react";
+import { RequestConfig, RestUrl } from "../../api/Config";
+import { WalletAddressResponse } from "../../api/Wallet";
 import { Box } from "../ui/Box";
 import { Card } from "../ui/Card";
 import { Container } from "../ui/Container";
 import { QRCode } from "../ui/QRCode";
 import { H1, H3 } from "../ui/Text";
+import axios from 'axios';
 
 export interface WalletViewProps {}
 
-export interface WalletViewState {}
+export interface WalletViewState {
+  walletAddress?: string;
+}
 
 export class WalletView extends Component<WalletViewProps, WalletViewState> {
-  private walletStealthAddress: string =
-    "L3D2VnognoqcZAFTtYVhFN4UNjyspBKbn1GxEEtr25uhynPGk6FWoGG5zhkBbhWnbuzvKpArwawqadGJeEzb83P7da9hudK2gvYZbw";
-  private walletAddress: string = "DFbNCTtvWx8eohBEoVJEFbBR47BFkqYSJz";
   constructor(props: WalletViewProps) {
     super(props);
     // bind events
     this.componentDidMount = this.componentDidMount.bind(this);
     this.componentWillUnmount = this.componentWillUnmount.bind(this);
+    this.getWalletAddresses = this.getWalletAddresses.bind(this);
+    this.getWalletAddresses();
   }
 
   componentDidMount(): void {}
 
   componentWillUnmount(): void {}
+
+  private getWalletAddresses = () => {
+    var self = this;
+    axios.get<WalletAddressResponse>(RestUrl + "wallet/defaultaddress", RequestConfig).then(function (response) {
+      self.setState( { walletAddress: response.data.result.address });
+    }).catch(function (error) {
+        console.log("Execute dynamic-cli JSON RCP [Get] Error: " + error);
+    });
+  };
 
   render() {
     return (
@@ -31,27 +44,31 @@ export class WalletView extends Component<WalletViewProps, WalletViewState> {
           Wallet Info
         </H1>
         <Container height="300vh">
+        {this.state &&  (
           <Box direction="column" align="center" width="100%">
             <Box display="flex" direction="row" align="center" width="100%">
               <Card
-                //onClick={() => this.props.restoreUsingMnemonic()}
                 padding="2em 1em 1em 1em"
                 min-height="140px"
                 min-width="220px"
               >
-                <H3 align="center" color="black">
-                  Stealth Address
-                </H3>
-                <QRCode
-                  bgColor="#00000000"
-                  fgColor="#4a4a4aff"
-                  ecLevel="M"
-                  minPadding={30}
-                  minimumCellSize={4}
-                  qrStyle="dots"
-                  value={this.walletStealthAddress}
-                  size={205}
-                />
+                {this.state.walletAddress && this.state.walletAddress.length > 0 && (
+                  <>
+                    <H3 align="center" color="black">
+                      Wallet Address
+                    </H3>
+                    <QRCode
+                      bgColor="#00000000"
+                      fgColor="#4a4a4aff"
+                      ecLevel="M"
+                      minPadding={30}
+                      minimumCellSize={4}
+                      qrStyle="dots"
+                      value={this.state.walletAddress}
+                      size={240}
+                    />
+                  </>
+                )}
                 <div
                   style={{
                     wordBreak: "break-all",
@@ -63,44 +80,12 @@ export class WalletView extends Component<WalletViewProps, WalletViewState> {
                     position: "relative"
                   }}
                 >
-                  <span>{this.walletStealthAddress} </span>
-                </div>
-              </Card>
-              <Card
-                //onClick={() => this.props.restoreUsingMnemonic()}
-                padding="2em 1em 1em 1em"
-                min-height="140px"
-                min-width="220px"
-              >
-                <H3 align="center" color="black">
-                  Wallet Address
-                </H3>
-                <QRCode
-                  bgColor="#00000000"
-                  fgColor="#4a4a4aff"
-                  ecLevel="M"
-                  minPadding={30}
-                  minimumCellSize={4}
-                  qrStyle="dots"
-                  value={this.walletAddress}
-                  size={240}
-                />
-                <div
-                  style={{
-                    wordBreak: "break-all",
-                    maxWidth: "500px",
-                    fontFamily: '"Courier New", Courier, monospace',
-                    textAlign: "center",
-                    color: "#4a4a4a",
-                    margin: "0 10px 10px",
-                    position: "relative"
-                  }}
-                >
-                  <span>{this.walletAddress} </span>
+                  <span>{this.state.walletAddress} </span>
                 </div>
               </Card>
             </Box>
           </Box>
+        )}
         </Container>
       </>
     );
